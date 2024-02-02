140a470783205ff64d8d3c834dfbd38d9692366e
JAADZUKA <a290605@alumnos.uaslp.mx>
2024-02-02 13:16:56 -0600

    Implementacion primer parte

--- compiladoresPr/Form1.Designer.cs
+++ compiladoresPr/Form1.Designer.cs
@@ -28,35 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textExpresion = new System.Windows.Forms.TextBox();
+            this.textPosfija = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(674, 109);
+            this.panel1.TabIndex = 0;
+            // 
+            // textExpresion
+            // 
+            this.textExpresion.Location = new System.Drawing.Point(55, 190);
+            this.textExpresion.Name = "textExpresion";
+            this.textExpresion.Size = new System.Drawing.Size(180, 22);
+            this.textExpresion.TabIndex = 1;
+            this.textExpresion.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textPosfija
+            // 
+            this.textPosfija.Location = new System.Drawing.Point(55, 264);
+            this.textPosfija.Name = "textPosfija";
+            this.textPosfija.Size = new System.Drawing.Size(180, 22);
+            this.textPosfija.TabIndex = 2;
+            this.textPosfija.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Analisi Lexico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Expresion regular:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Posfija:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(403, 34);
+            this.button1.Location = new System.Drawing.Point(91, 218);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convertir a posfija";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(135, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(381, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Identificador Fundamentos de Compiladores-Deadline";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Miguel Alejandro Gutierrez Silva";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(238, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Juan Daniel Avalos Alaniz";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(672, 466);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPosfija);
+            this.Controls.Add(this.textExpresion);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textExpresion;
+        private System.Windows.Forms.TextBox textPosfija;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- compiladoresPr/Form1.cs
+++ compiladoresPr/Form1.cs
@@ -12,12 +12,114 @@ namespace compiladoresPr
 {
     public partial class Form1 : Form
     {
+        //Declaración de variables
+        private Stack<char> pila; // Pila para almacenar los operadores
+        private Queue<char> posfija; // Cola para almacenar la salida posfija
+        private Dictionary<char, int> precedencia; // Diccionario para almacenar la precedencia de los operadores
+
         public Form1()
-        {
+        { 
             InitializeComponent();
+            // Inicialización de variables
+            pila = new Stack<char>();
+            posfija = new Queue<char>();
+            precedencia = new Dictionary<char, int>();
+            // Asignación de la precedencia de los operadores
+            //Defincion de prioridad de operadores
+            precedencia.Add('(', 0);
+            //precedencia.Add(')', 3);
+            precedencia.Add('+', 1);
+            precedencia.Add('-', 1);
+            precedencia.Add('*', 2);
+            precedencia.Add('/', 2);
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Obtener la expresión regular del textbox
+            string expresion = textExpresion.Text;
+            // Limpiar la pila y la cola
+            pila.Clear();
+            posfija.Clear();
+            // Recorrer cada caracter de la expresión
+            foreach (char c in expresion)
+            {
+                // Si es un operando, lo agregamos a la cola
+                //funcion para checar que c no se encuentre en la lista de precedencia
+
+                if (!precedencia.ContainsKey(c))
+                {
+                    posfija.Enqueue(c);
+                }
+                /*// Si es un paréntesis izquierdo, lo agregamos a la pila
+                else if (c == '(' || c ==')')
+                {
+                    pila.Push(c);
+                }
+                // Si es un paréntesis derecho, desapilamos hasta encontrar el paréntesis izquierdo
+                else if (c == ')')
+                {
+                    while (pila.Peek() != '(')
+                    {
+                        posfija.Enqueue(pila.Pop());
+                    }
+                    pila.Pop(); // Eliminamos el paréntesis izquierdo
+                }*/
+                // Si es un operador, lo comparamos con el tope de la pila
+                else if (precedencia.ContainsKey(c))
+                {
+                    // Si la pila está vacía o el operador tiene mayor precedencia que el tope, lo apilamos
+                    if (pila.Count == 0 || precedencia[c] > precedencia[pila.Peek()])
+                    {
+                        pila.Push(c);
+                    }
+                    // Si el operador tiene menor o igual precedencia que el tope, desapilamos hasta que se cumpla lo contrario y luego apilamos el operador
+                    else
+                    {
+                        while (pila.Count > 0 && precedencia[c] <= precedencia[pila.Peek()])
+                        {
+                            posfija.Enqueue(pila.Pop());
+                        }
+                        pila.Push(c);
+                    }
+                }
+            }
+            // Al terminar de recorrer la expresión, desapilamos todo lo que queda en la pila y lo agregamos a la cola
+            while (pila.Count > 0)
+            {
+                posfija.Enqueue(pila.Pop());
+            }
+            // Mostramos la salida posfija en el textbox correspondiente
+            textPosfija.Text = string.Join("", posfija.ToArray());
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
