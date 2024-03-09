6081c8e9f9cfdfbcc9fb11da0286bd0cac0246a8
Mags10 <116739180+Mags10@users.noreply.github.com>
2024-03-08 18:54:50 -0600

    Transiciones (EpsilonClosure) correción. EpsilonTransitions -> instrucciones.

--- compiladoresPr/Formularios/TransitionTable.Designer.cs
+++ compiladoresPr/Formularios/TransitionTable.Designer.cs
@@ -47,7 +47,6 @@
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(539, 484);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
